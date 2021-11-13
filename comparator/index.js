3e69b9c0dc448a91af999b08399b2a6b994e5ed5
Amirouche <amirouche@hyper.dev>
2021-11-13 12:36:20 +0100

    comparator: clean, let's restart from scratch.

--- comparator/index.js
+++
@@ -1,7 +0,0 @@
-const h = require('react-hyperscript');
-const React = require('react');
-const ReactDOM = require('react-dom');
-const hh = require('hyperscript-helpers')(h);
-
-
-export default {h, ReactDOM};
