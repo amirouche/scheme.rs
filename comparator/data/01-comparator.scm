95c46226aac9872afc5a58b9567c3f01a9191380
Amirouche <amirouche@hyper.dev>
2021-11-13 12:35:21 +0100

    comparator: add initial data

---
+++ comparator/data/01-comparator.scm
@@ -0,0 +1,31 @@
+;; hyper data, or metadata renaissance
+
+(r7rs type filter)
+(r7rs name "R7RS")
+
+(top3 type filter)
+(top3 name "Top 3 in ecraven's benchmarks")
+
+(cffi type filter)
+(cffi name "C Foreign-Function Interface")
+
+(binary-executables type filter)
+(binary-executables name "Create binary executables")
+
+(c-embedding type filter)
+(c-embedding name "Embedded in C program")
+
+(sicp type filter)
+(sicp name "Tools to ease the practice of SICP")
+
+(server-side-webdev type filter)
+(server-side-webdev name "Backend webdev")
+
+(frontend-side-webdev type filter)
+(frontend-side-webdev name "Frontend webdev")
+
+(doc50k type filter)
+(doc50k name "50,000 words of documentation")
+
+(operating-system type filter)
+(operating-system name "Operating-System")
